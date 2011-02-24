4e91a716898ea86b830b3edf43b78dc68555943f
Bob Karreman <bob@bob-karreman.(none)>
2011-02-24 12:32:22 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-form_designer',
+    version='1.0',
+    description='Django form designer',
+    long_description='',
+    author='bobkarreman',
+    author_email='',
+    url='https://github.com/bobkarreman/django-form-designer',
+    packages=find_packages(),
+    classifiers=[],
+    include_package_data=True,
+    zip_safe=False,
+)
